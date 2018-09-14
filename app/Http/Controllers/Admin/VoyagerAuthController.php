b8aacdef0dfe70574f99bfa5324966b96f99542c
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2018-09-14 01:40:27 -0600

    Se ha colocado la plantilla y paquetes para la Administracion

---
+++ app/Http/Controllers/Admin/VoyagerAuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use TCG\Voyager\Http\Controllers\VoyagerAuthController as BaseVoyagerAuthController;
+
+class VoyagerAuthController extends BaseVoyagerAuthController
+{
+    public function username()
+    {
+        return 'username';
+    }
+}
