9b32c45c9cce0917531adb9513b3170576c5d616
Jose Victor Pacajoj Mejia <jovipac@hotmail.com>
2018-09-15 01:43:16 -0600

    Se trabajado en el modulo de Usuarios

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         TCG\Voyager\VoyagerServiceProvider::class,
+        Laravolt\Avatar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Avatar'    => Laravolt\Avatar\Facade::class,
     ],
 
 ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('name');            
             $table->string('email')->unique();
+            $table->string('avatar')->nullable()->default('images/default_avatar.png');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
